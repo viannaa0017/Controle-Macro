ad096994fee9ac927a6bdc703be3b17a815c3886
Victor Viana <viana.victor0017@gmail.com>
2026-07-02 16:32:48 -0300

    api render

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
-const express =
-    require('express');
+const express = require('express');
+const cors = require('cors');
 
-const cors =
-    require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 const fs =
     require('fs');
@@ -630,17 +632,8 @@ app.delete(
 /* PORTA */
 /* ===================================== */
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.listen(
-    PORT,
-    () => {
-
-        console.log(
-
-            `Servidor rodando na porta ${PORT}`
-
-        );
-
-    }
-);
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
